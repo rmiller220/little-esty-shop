a60fee0f3e52583a1a56d3699b43b3ce2a95ae81
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-25 12:00:20 -0600

    add bulk_discount destroy all

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+BulkDiscount.destroy_all
 InvoiceItem.destroy_all
 Transaction.destroy_all
 Invoice.destroy_all
